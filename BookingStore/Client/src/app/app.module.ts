49beae8935f11ec7deefeb522ba8524d489ead5c
Daniel Pretlove <danielpretlove5@gmail.com>
2022-05-18 08:01:35 +1000

    added component logic with login

--- BookingStore/Client/src/app/app.module.ts
+++ BookingStore/Client/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
-      {path: '', component: LoginComponent, pathMatch: 'full'}
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'register', component: RegisterComponent, pathMatch: 'full'}
     ])
   ],
   providers: [],
--- BookingStore/Client/src/app/login/login.component.ts
+++ BookingStore/Client/src/app/login/login.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Directive, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+  
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+  constructor(private router: Router) { 
+
+  }
+  
+  public LoginClick = (): void => {
+    if (this.username === "Daniel" && this.password === "Gintama22") {
+      this.router.navigateByUrl("/register")
+      console.log("AAAAAAAAAAAAAAA")
+    } else {
+      console.log("WHAT THE FUCK");
+    }
+  }
 
   ngOnInit(): void {
   }
